2eb322c4bb3391dd58be824e9ae3b9de8e279c05
Vladimir Ignatov <vlad.ignatov@gmail.com>
2020-05-27 08:12:08 -0400

    Use dotenv config

--- dbPool.js
+++ dbPool.js
@@ -1,13 +1,4 @@
-const mysql = require("mysql2/promise");
+const mysql  = require("mysql2/promise");
+const config = require("./config");
 
-// We connect to MySQL database that is hosted on Google Cloud and is publicly
-// accessible in read-only mode
-module.exports = mysql.createPool({
-    host              : "35.221.49.252",
-    user              : "bulk-data-read-only-user",
-    password          : "nq9FkLs}btq]mP8ZW8}c4ke)",
-    database          : "bulk-data-stu-3",
-    waitForConnections: true,
-    connectionLimit   : 10,
-    queueLimit        : 0
-});
+module.exports = mysql.createPool(config.db);
